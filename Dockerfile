eb1b9fb188a9d6516e25454ceee34fb3960c6cdf
rubenmodamioin2 <ruben.modamio@in2.es>
2023-11-09 09:04:17 +0100

    modified the dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ARG SKIP_TESTS=false
 COPY build.gradle settings.gradle /home/gradle/src/
 COPY src /home/gradle/src/src
 COPY gradle /home/gradle/src/gradle
-COPY configs /home/gradle/src/configs
+COPY waltid/configs /home/gradle/src/waltid/configs
 COPY service-matrix.properties /home/gradle/src/
 WORKDIR /home/gradle/src
 RUN if [ "$SKIP_TESTS" = "true" ]; then \
@@ -15,9 +15,9 @@ RUN if [ "$SKIP_TESTS" = "true" ]; then \
   fi
 
 # build image
-FROM openjdk:17-alpine
+FROM eclipse-temurin:17.0.8.1_1-jre-jammy
 WORKDIR /app
-COPY --from=TEMP_BUILD /home/gradle/src/service-matrix.properties /app/
-COPY --from=TEMP_BUILD /home/gradle/src/configs /app/configs
 COPY --from=TEMP_BUILD /home/gradle/src/build/libs/*.jar /app/
-ENTRYPOINT ["java", "-jar", "/app/wallet-crypto-0.0.1.jar"]
+COPY --from=TEMP_BUILD /home/gradle/src/service-matrix.properties /app/
+COPY --from=TEMP_BUILD /home/gradle/src/waltid/configs /app/waltid/configs
+ENTRYPOINT ["java", "-jar", "/app/wallet-crypto-0.0.1-SNAPSHOT.jar"]
--- src/main/java/es/in2/wallet/crypto/controller/CredentialController.java
+++ src/main/java/es/in2/wallet/crypto/controller/CredentialController.java
@@ -1,31 +1,31 @@
-//package es.in2.wallet.crypto.controller;
-//
-//import es.in2.wallet.crypto.service.VaultService;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@Slf4j
-//@RestController
-//@RequestMapping("/api/v1/credentials")
-//@RequiredArgsConstructor
-//public class CredentialController {
-//
-//    private final VaultService vaultService;
-//
-//    @PostMapping
-//    public void addCredential() {
-//        vaultService.addCredential();
-//    }
-//
-//    @GetMapping
-//    public void getCredential() {
-//        log.info("getCredential()");
-//        vaultService.getCredential();
-//    }
-//
-//
-//}
+package es.in2.wallet.crypto.controller;
+
+import es.in2.wallet.crypto.service.VaultService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/credentials")
+@RequiredArgsConstructor
+public class CredentialController {
+
+    private final VaultService vaultService;
+
+    @PostMapping
+    public void addCredential() {
+        vaultService.addCredential();
+    }
+
+    @GetMapping
+    public void getCredential() {
+        log.info("getCredential()");
+        vaultService.getCredential();
+    }
+
+
+}
